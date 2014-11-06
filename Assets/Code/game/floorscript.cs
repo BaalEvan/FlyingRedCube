6be0f964b9247c6d754b4f98c886c559b4422e41
Norbert Rutkowski <norutkow@gmail.com>
2014-11-06 12:32:27 +0100

    Update!

---
+++ Assets/Code/game/floorscript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class floorscript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+				transform.position = new Vector3 (transform.position.x - 0.08f, transform.position.y, transform.position.z);
+		
+		if (transform.position.x < -24.19352f) {
+			transform.position = new Vector2 (1.67462f, transform.position.y);
+				}
+		}
+}
