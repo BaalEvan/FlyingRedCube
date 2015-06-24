15a7b72409317cbfbe898e1aa264571b2f0e6a7b
Norbert Rutkowski <norutkow@gmail.com>
2015-06-24 23:28:18 +0200

    Poprawione Menu, naprawiony SoundBtn, wstępnie wszystko ustawione - brak grafik

--- Assets/SoundController.cs
+++ Assets/SoundController.cs
@@ -1,59 +1,33 @@
-﻿
-
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
 public class SoundController : MenuButtonClass
-{ 
+{
 
     public GameObject soundoff;
     public GameObject sound;
     public bool dzwiekwlacz;
     public ButtonController BC;
+    public GameObject soundBtnPlay;
+    public GameObject soundBtnStop;
+
 
     // Use this for initialization
     void Start()
     {
         int cos = PlayerPrefs.GetInt("soundset", 0);
-        sound.SetActive(true);
 
         if (cos == 0)
         {
-            //soundoff.SetActive(false);
+            soundBtnPlay.SetActive(true);
+            BC.soundoffbtn = true;
             Debug.Log("soundoff false");
         }
         else
         {
-            //soundoff.SetActive(true);
-            AudioListener.pause = true;
+            BC.soundoffbtn = false;
+            soundBtnStop.SetActive(true);
             Debug.Log("soundoff true");
         }
-
-        
-    }
-
-    void Update()
-    {
-        if (BC.soundoffbtn == false)
-        {
-
-            dzwiekwlacz = false;
-            //soundoff.SetActive(true);
-            AudioListener.pause = true;
-            PlayerPrefs.SetInt("soundset", 1);
-
-        }
-        else
-        {
-
-            dzwiekwlacz = true;
-            AudioListener.pause = false;
-            //soundoff.SetActive(false);
-            PlayerPrefs.SetInt("soundset", 0);
-        }
-
     }
 }
-
-
-
